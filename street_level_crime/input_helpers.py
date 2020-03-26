0d0dd6eb5f08bd69f2eacccde25eaa1ecbf52d17
Artemijs <artemak2@gmail.com>
2020-03-26 12:42:14 +0000

    Edits
    1. Corrected file validation and made code to check if the file exists in a directory  2. Put RegEx into the functions they're referenced in 3. Combined get_folder_path and get_file_name (don't think we need to collect them separately?) to return full path to a file + '.csv' extension 4. Added folder path formatting (so when user enters C:\path\to\file it changes it to C:\path\to\file\)

--- street_level_crime/input_helpers.py
+++ street_level_crime/input_helpers.py
@@ -1,13 +1,34 @@
 import re
+import os
 
-uk_postcode_format = re.compile("([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})")
-valid_file_name = re.compile('[^\\\<>:"/|?*]')
 
 def validate_postcode(postcode):
+    uk_postcode_format = re.compile("([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})")
     return bool(uk_postcode_format.match(postcode))
 
 def validate_file_name(file_name):
+    valid_file_name = re.compile('^[^\\\<>:"/|?]+$')
     return bool(valid_file_name.match(file_name))
+
+def directory_exists(directory):
+    if os.path.isdir(directory):
+        return True
+    else:
+        return False
+
+def directory_formatting(directory):
+    if directory[-1] != "\\" and directory[-1] != "/":
+        if "\\" in directory:
+            directory = directory + "\\"  
+        elif "/" in directory:
+            directory = directory + "/"        
+    return directory
+    
+def file_exists(path_to_file):
+    if os.path.isfile(path_to_file):
+        return True
+    else:
+        return False
 
 def ui_get_postcode():
     while True:
@@ -35,29 +56,29 @@ def ui_get_distance():
 def ui_get_sort_options():
     while True:
         option = input("Please, select sort option (number 1 to 3): \n 1. By disatnce from the postcode centre \n 2. By date (most recent first) \n 3. Crime category \n")
-        if option == str(1) or option == str(2) or option == str(3):
+        if option in ["1", "2", "3"]:
             break
         else:
             print("Please, select a number from 1 to 3")
     return option
 
-def ui_get_file_name():
+def ui_get_file_and_directory():
     while True:
-        file_name = input("Please enter a file name for report")
-        if validate_file_name(file_name):
+        path_to_directory = input("Please, enter a path to destination folder: ")
+        if directory_exists(path_to_directory):
+            print("Path accepted")
+            break
+        else:
+            print("Path to directory doesn't exist. Please, try again.")
+        
+    while True:
+        file_name = input("Please enter a file name: ")
+        if not file_exists(path_to_directory + file_name) and validate_file_name(file_name):
             print("File name accepted")
-            return file_name
+            return directory_formatting(path_to_directory) + file_name + ".csv"
+        
+        elif file_exists(path_to_directory + file_name):
+            print("File already exists. Please, try different name")       
         else:
             print("Wrong! Path cant contain the following characters: < (less than) , > (greater than) , : (colon) , \" (double quote) , / (forward slash)  , \\ (backslash) , | (vertical bar or pipe) \n ? (question mark) , * (asterisk)")
-
-def ui_get_directory_name():
-    while True:
-        directory_name = input("Please, enter a path to destination folder")
-        if validate_file_name(directory_name):
-            print("Path accepted")
-            return directory_name
-        else:
-            print("Wrong! Path cant contain the following characters: < (less than) , > (greater than) , : (colon) , \" (double quote) , / (forward slash)  , \\ (backslash) , | (vertical bar or pipe) \n ? (question mark) , * (asterisk) ")
-
         
-#Function to produce a tabular report
