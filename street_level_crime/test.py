3010b90a47a8b928a14eb3b37b1d79ad3a1a38bc
Emily Peregrine <emily.peregrine1@nhs.net>
2020-03-26 11:11:39 +0000

    Added geodist

--- street_level_crime/test.py
+++ street_level_crime/test.py
@@ -1,5 +1,6 @@
 import unittest
 import input_helpers
+import geodist
 
 class UIHelpersTests(unittest.TestCase):
     def test_valid_postcode_no_spaces(self):
@@ -17,6 +18,32 @@ class UIHelpersTests(unittest.TestCase):
     def test_invalid_postcode(self):
         isValid = input_helpers.validate_postcode("APPLE")
         self.assertFalse(isValid)
+
+uni_campus = (50.736544, -3.534948)
+exeter_cathedral = (50.722548, -3.529915)
+exeter_science_park = (50.731379, -3.455758)
+
+
+class GeodistTest(unittest.TestCase):
+    def test_dist_uni_to_cathedral(self):
+        #~= 1.6km google maps
+        dist = geodist.distance(uni_campus, exeter_cathedral)
+        self.assertAlmostEqual(dist, 1.6, delta=0.01)
+
+    def test_dist_same_location(self):
+        dist = geodist.distance(uni_campus, uni_campus)
+        self.assertEqual(0, dist)
+
+    def test_not_within_radius(self):
+        #5.31 on google maps
+        isWithin = geodist.is_within_radius(exeter_cathedral, exeter_science_park, 5.3)
+        self.assertFalse(isWithin)
+    
+    def test_within_radius(self):
+        #5.31 on google maps
+        isWithin = geodist.is_within_radius(exeter_cathedral, exeter_science_park, 5.4)
+        self.assertTrue(isWithin)
+
 
 if __name__ == '__main__':
     unittest.main()
