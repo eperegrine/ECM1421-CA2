a44b35d85e67a7cc1ec9d19cd5b3b782bee9b623
JackPlowman <jack.plowman1@nhs.net>
2020-03-26 20:06:35 +0000

    adding muti layered arrays

--- street_level_crime/createCSV.py
+++ street_level_crime/createCSV.py
@@ -1,28 +1,35 @@
 import csv
+
 rowData = []
+columnHeaders = []
 
-def convertToCSV(data, rowCount,rowLength):
-    rowNumber = 2
-    with open('crime data.csv','w', newline='') as f:
-        csvWritter = csv.writer(f)
-        headers = data[0:rowLength]
-        csvWritter.writerow(headers)
-        try:
-            for i in range (1,rowCount+1,1):
-                splitRows(data, rowLength, i)
-                csvWritter.writerow(rowData)
-                rowNumber = rowNumber + 1
-        except:
-            pass
-            
+
+def convertToCSV(data):
+    try:
+        with open('crime data.csv', 'w', newline='') as f:
+            csvWritter = csv.writer(f)
+            columnHeaders[:] = data[0]
+            rowLength = len(columnHeaders[:])
+            rowCount = len(data[1]) / rowLength
+            data[:] = data[1]
+            csvWritter.writerow(columnHeaders)
+            try:
+                for i in range(1, int(rowCount) + 1, 1):
+                    splitRows(data, rowLength, i)
+                    csvWritter.writerow(rowData)
+                    rowNumber = rowNumber + 1
+            except:
+                print("Error writing to CSV")
+                csvWritter.writerow("Error writing to CSV")
+    except:
+        print("Data error")
+
+
 def splitRows(data, rowLength, rowNumber):
-    firstPositionInRow = rowLength*rowNumber
-    lastPositionInRow = rowLength*(rowNumber+1)
-    rowData[:]= data[firstPositionInRow:lastPositionInRow]
+    firstPositionInRow = (rowLength * rowNumber)-rowLength
+    lastPositionInRow = (rowLength * rowNumber)
+    rowData[:] = data[firstPositionInRow:lastPositionInRow]
 
 
-
-data = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
-rowCount = 4
-rowLength = 3
-convertToCSV(data, rowCount, rowLength)
+data = [['a', 'b', 'c'], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']]
+convertToCSV(data)
