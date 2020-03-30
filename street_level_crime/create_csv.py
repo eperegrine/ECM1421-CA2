e2afd594196bf1cacb45d9887afd6cd0d22ca4cb
JackPlowman <jack.plowman1@nhs.net>
2020-03-30 15:37:44 +0100

    adding customer paths

--- street_level_crime/create_csv.py
+++ street_level_crime/create_csv.py
@@ -1,4 +1,6 @@
 import csv
+import shutil
+import os
 
 
 def convert_to_csv(headers: tuple, data: list) -> csv:
@@ -11,13 +13,26 @@ def convert_to_csv(headers: tuple, data: list) -> csv:
     filename = input("Please enter a filename: ")
     special_commands(filename)
     try:
-        with open(filename + '.csv', 'w', newline='') as f:  # creates document and opens for writer object
+        with open(filename + '.csv', 'w+', newline='') as f:  # creates document and opens for writer object
             csvWriter = csv.writer(f)  # creates writer object
             csvWriter.writerow(headers)  # writes header
             for i in range(0, len(data) - 1, 1):  # writes each row in array
                 csvWriter.writerow(data[i])  # writes from
             print("File Created:", filename + ".csv")
             f.close()
+            try:
+                src = os.path.dirname(os.path.realpath(filename + ".csv"))+'\\' + filename + ".csv"
+                home = os.path.expanduser("~")
+                dst = home + "\\" + input("Please enter a directory: "+ home + "\\")
+                special_commands(dst)
+                if os.path.exists(dst):
+                    shutil.move(src,dst)
+                else:
+                    print("Saved to default location: " + src)
+            except:
+                print("Unable to save file to custom path")
+                print("Saved to " + src)
+
     except:
         print("Error writing to CSV")
         csvWriter.writerow("Error writing to CSV")
@@ -25,7 +40,7 @@ def convert_to_csv(headers: tuple, data: list) -> csv:
 
 def special_commands(variable):
     if variable == "q" or "h" or "r" or "quit" or "help" or "restart":
-        quit_help_restart_function()
+        pass
 
 
 headers = ['a', 'b', 'c']
