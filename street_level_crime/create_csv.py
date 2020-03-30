2c26f3bdd56a0a794b9a60e36d8b89d5e66e3588
JackPlowman <jack.plowman1@nhs.net>
2020-03-30 10:56:24 +0100

    rewrote whole function to take in changed input

---
+++ street_level_crime/create_csv.py
@@ -0,0 +1,29 @@
+import csv
+
+rowData = []
+columnHeaders = []
+
+
+def new_convert_to_csv(headers: tuple, data: list) -> csv:
+    """
+ Converts variables to csv
+    :Tuple headers: Contians the headers of the csv file
+    :List data: Contains a list of row data in format List[List[String]]
+    """
+    try:
+        rowNumber = 0
+        with open('crime data.csv', 'w', newline='') as f:
+            csvWriter = csv.writer(f)
+            csvWriter.writerow(headers)
+
+            for i in range(0, len(data) - 1, 1):
+                csvWriter.writerow(data[i])
+
+    except:
+        print("Error writing to CSV")
+        csvWriter.writerow("Error writing to CSV")
+
+
+headers = ['a', 'b', 'c']
+data = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o']]
+new_convert_to_csv(headers, data)
