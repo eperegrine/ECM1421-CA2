ff05223cbe6ea462e36bbd08d5304586945a0101
JackPlowman <jack.plowman1@nhs.net>
2020-03-30 11:04:13 +0100

    renaming function and adding more documentation

--- street_level_crime/create_csv.py
+++ street_level_crime/create_csv.py
@@ -1,23 +1,18 @@
 import csv
 
-rowData = []
-columnHeaders = []
 
-
-def new_convert_to_csv(headers: tuple, data: list) -> csv:
+def convert_to_csv(headers: tuple, data: list) -> csv:
     """
  Converts variables to csv
     :Tuple headers: Contians the headers of the csv file
     :List data: Contains a list of row data in format List[List[String]]
     """
     try:
-        rowNumber = 0
-        with open('crime data.csv', 'w', newline='') as f:
-            csvWriter = csv.writer(f)
-            csvWriter.writerow(headers)
-
-            for i in range(0, len(data) - 1, 1):
-                csvWriter.writerow(data[i])
+        with open('crime data.csv', 'w', newline='') as f:  # creates document and opens for writer object
+            csvWriter = csv.writer(f)  # creates writer object
+            csvWriter.writerow(headers)  # writes header
+            for i in range(0, len(data) - 1, 1):  # writes each row in array
+                csvWriter.writerow(data[i])  # writes from
 
     except:
         print("Error writing to CSV")
